f4d7a97c7984a42428a27944d3177b54a3d28c24
Ivanilson-Costa18 <ivanilsonpittafros@gmail.com>
2020-11-13 21:15:35 +0000

    kertana update

---
+++ kertanaCode/public/javascripts/studentGrades.js
@@ -0,0 +1,15 @@
+var name
+name += "grades"
+
+window.onload = () =>{
+    document.getElementById(student).innerHTML = name;
+} 
+
+const subject = (name,grade) => {
+    this.name=name;
+    this.grade=grade;
+}
+
+subject(Mathematics,7.5)
+subject(Literature,11.2)
+
