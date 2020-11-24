447d80acabfe69b21874a1a99eaf192469d075be
Ivanilson-Costa18 <ivanilsonpittafros@gmail.com>
2020-11-24 12:51:35 +0000

    index.html and map.html updates
    Index.html: Search button created
    Map.html: Draw polygon inserted and map display fixes

---
+++ kertanaCode/public/javascripts/displayMap.js
@@ -0,0 +1,9 @@
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiaXZhbnBnIiwiYSI6ImNraGwybDczMzFnOXcyeHA2MnM0ZWF4aDQifQ.dbfnIhEI5JJf-TV1LyEQQw';
+var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/ivanpg/ckhp1ckfr2dbd19o0op09umzk', 
+        center: [-9.142685, 38.736946], 
+        zoom: 9
+        });
+
