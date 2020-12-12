67c5f82d0c7fe5d523624c2ad67b66caddae4dca
Ivanilson-Costa18 <ivanilsonpittafros@gmail.com>
2020-12-12 18:45:29 +0000

    Filter function update (index.html)

--- kertanaCode/public/javascripts/searchBar.js
+++ kertanaCode/public/javascripts/searchBar.js
@@ -9,47 +9,77 @@ async function loadListProducts() {
             method: "get",
             dataType: "json"
         });
-        showProducts(products);
+        getSuggestions(products);
         
     } catch(err) {
         console.log(err);
     }
 }
 
-
-function showProducts(products) {
-    let elemMain = document.getElementById("list");
-    let html ="";
+let suggestions = [];
+function getSuggestions(products) {
     for (let product of products) {
-        html += '<li class="animals">'+product.nome+'</li>';
+    suggestions.push(`${product.nome}`) 
     }
-    elemMain.innerHTML = html;
 }
 
 
-function showProduct(productID) {
-    sessionStorage.setItem("productID",productID);
+function showProduct(productNome) {
+    sessionStorage.setItem("productNome",productNome);
     window.location = "map.html";
 }
 
-async function filtrar() {
-    try {
-        let hortalica = document.getElementById("search-hortalica").value;
-        let localizacao = document.getElementById("search-localizacao").value;
-        if (hortalica == null) {
-            let products = await $.ajax({
-            url: '/api/localizations?artist='+localizacao,
-            method: "get",
-            dataType: "json"
-        })} else {
-            let products = await $.ajax({
-                url: "/api/products?product="+hortalica,
-                method: "get",
-                dataType: "json"
-        })};
-        showProducts(products);       
-    } catch(err) {
-        console.log(err);
+const searchWrapper = document.querySelector(".search-input");
+const inputBox = searchWrapper.querySelector("input");
+const suggBox = searchWrapper.querySelector(".autocom-box");
+const icon = searchWrapper.querySelector(".icon");
+let linkTag = searchWrapper.querySelector("a");
+let webLink;
+
+// if user press any key and release
+inputBox.onkeyup = (e)=>{
+    let userData = e.target.value; //user enetered data
+    let emptyArray = [];
+    if(userData){
+        icon.onclick = ()=>{
+            showProduct(selecData)
+        }
+        emptyArray = suggestions.filter((data)=>{
+            //filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
+            return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase()); 
+        });
+        emptyArray = emptyArray.map((data)=>{
+            // passing return data inside li tag
+            return data = '<li>'+ data +'</li>';
+        });
+        searchWrapper.classList.add("active"); //show autocomplete box
+        showSuggestions(emptyArray);
+        let allList = suggBox.querySelectorAll("li");
+        for (let i = 0; i < allList.length; i++) {
+            //adding onclick attribute in all li tag
+            allList[i].setAttribute("onclick", "select(this)");
+        }
+    }else{
+        searchWrapper.classList.remove("active"); //hide autocomplete box
     }
 }
- 
+
+function select(element){
+    let selectData = element.textContent;
+    inputBox.value = selectData;
+    icon.onclick = ()=>{
+        showProduct(selecData)
+    }
+    searchWrapper.classList.remove("active");
+}
+
+function showSuggestions(list){
+    let listData;
+    if(!list.length){
+        userValue = inputBox.value;
+        listData = '<li>'+ userValue +'</li>';
+    }else{
+        listData = list.join('');
+    }
+    suggBox.innerHTML = listData;
+}
